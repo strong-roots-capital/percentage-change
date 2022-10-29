edf7062f1882663aadf6b7d6e6a4bf9fda7da7ec
Eric Crosson <eric.s.crosson@utexas.edu>
2022-10-29 13:08:57 -0500

    chore: reduce flake inputs using follows

--- flake.nix
+++ flake.nix
@@ -2,8 +2,13 @@
   description = "od";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    pre-commit-hooks.url = "github:ericcrosson/pre-commit-hooks.nix/add-eslint";
+    pre-commit-hooks = {
+      url = "github:ericcrosson/pre-commit-hooks.nix/add-eslint";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
